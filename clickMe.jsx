2c3c4bfbd840a41e836331fbb4fff4fbb071b47e
ShahrukhRahman94 <rahmanshahrukh101@gmail.com>
2024-05-21 16:01:50 +0530

    day 24

---
+++ clickMe.jsx
@@ -0,0 +1,16 @@
+
+
+const handleOnClick = ()=>{
+
+
+}
+const App = () => {
+
+
+
+
+	// Edit this component
+	return <button data-testid="button" onClick={(e)=>handleOnClick(console.log("Clicked!"))}>Click me!</button>
+}
+
+export default App
